96ed8b917e34199a2d65eb8fc761ad34d2b2fbdc
Terje Berg-Hansena <terje@itfakultetet.no>
2019-01-02 16:34:46 +0100

    Updatering

--- Ord/src/no/itfakultetet/App.java
+++ Ord/src/no/itfakultetet/App.java
@@ -10,29 +10,39 @@ public class App {
 	 */
 	public static void main(String[] args) {
 
-		// Hent filnavnet fra kommandolinjens første argument (det første vi
-		// skriver inn) og lagre filnavnet i variablen fil
-
+		// Sjekk at det gis ett argument til programmet eller opplys om bruken
+		
 		if (args.length != 1) {
 			System.out.println("Bruk: java -Jar Ord.jar <tekstfil>");
 			System.exit(0);
 		}
 
+		
+		// Hent filnavnet fra kommandolinjens første argument (det første vi
+		// skriver inn) og lagre filnavnet i variablen fil
+
 		String fil;
 		fil = args[0];
 
+		// Instansier et Ord-objekt
 		Ord ord = new Ord();
 		ord.setFil(fil);
+		
+		// Les inn filen fra argumentet til programmet og gi en melding om at vi behandler filen
 		System.out.println("Behandler " + ord.getFil());
+		
+		// start en tidtaker
 		Long start = System.currentTimeMillis();
 		ord.setUtfil1("ord_" + fil);
 		ord.setUtfil2("antall_" + fil);
-
+		
+		// Lag to Maps hvor vi kan mellomlagre ordene i tekstfilen vi leser inn 
 		Map<String, Integer> tre = ord.lesFil(ord.getFil());
 		Map<String, Integer> sortert = ord.sorterFil(tre);
 	
 		// Skriv fil med ord i alfabetisk orden
 		ord.skrivfil(ord.getUtfil1(), tre);
+		
 		// Skriv fil med ord sortert etter antall
 		ord.skrivfil(ord.getUtfil2(), sortert);
 
