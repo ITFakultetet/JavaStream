4729d10c3173a24431df12a75026f072fa351f2d
Terje Berg-Hansena <terje@itfakultetet.no>
2019-01-31 22:07:50 +0100

    List og LinkedList

---
+++ List_LinkedList/src/no/itfakultetet/ListLinkedList.java
@@ -0,0 +1,36 @@
+package no.itfakultetet;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+
+public class ListLinkedList {
+
+	public static void main(String[] args) {
+
+		List<Integer> tall1 = new ArrayList<Integer>();
+		List<Integer> tall2 = new LinkedList<Integer>();
+
+		long start = System.currentTimeMillis();
+
+//		Bytt ut tall1 med tall2 og se forskjellen på 
+//		ArrayList og LinkeList når vi setter inn nye rader fra 
+//		begynnelsen av listen ved add(0,i) 0 = fra begynnelsen.
+//		Tar vi bort 0, legges nye rader til fra slutten, og det er liten forskjell
+		
+		for (int i = 0; i < 3e5; i++) {
+			tall1.add(0, i);
+
+		}
+
+		long stopp = System.currentTimeMillis();
+		
+		System.out.println("Vi har lagt til "+3e5+" elementer i listen");
+		
+		System.out.println("Tid: " + (stopp - start)+ " millisekunder");
+		
+
+	}
+
+}
