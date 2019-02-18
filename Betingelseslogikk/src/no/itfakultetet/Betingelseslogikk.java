7c093b13a22c3075969cbc53b36c3c9770ff0549
terje <terje@DESKTOP-72SIH39>
2019-02-18 12:59:05 +0100

    Betingelseslogikk

--- Betingelseslogikk/src/no/itfakultetet/Betingelseslogikk.java
+++ Betingelseslogikk/src/no/itfakultetet/Betingelseslogikk.java
@@ -5,17 +5,18 @@ public class Betingelseslogikk {
 	public static void main(String[] args) {
 
 		// If-then-else
-		int a = 40;
+		Integer a = 40;
 
 		if (a < 20) {
-			System.out.println("a er mindre enn 20");
+			System.out.println(a + " er mindre enn 20");
 		} else {
-			System.out.println("a er ikke mindre enn 20");
+			System.out.println(a + " er ikke mindre enn 20");
 		}
 
 		// ternary operator
 
-//		a<40? System.out.println("a er mindre enn 40") : System.out.println("a er ikke mindre enn 40");
+		String resulat = a < 50 ? a + " er mindre enn 50" : a + " er ikke mindre enn 50";
+		System.out.println(resulat);
 	}
 
 }
