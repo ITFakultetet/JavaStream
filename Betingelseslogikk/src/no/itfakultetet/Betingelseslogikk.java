309b7548ddc8c815182d973f13c6574c7fb8cac9
terjebh <terjebh@W520>
2019-03-08 00:06:30 +0100

    Betingelseslogikk

--- Betingelseslogikk/src/no/itfakultetet/Betingelseslogikk.java
+++ Betingelseslogikk/src/no/itfakultetet/Betingelseslogikk.java
@@ -17,6 +17,31 @@ public class Betingelseslogikk {
 
 		String resulat = a < 50 ? a + " er mindre enn 50" : a + " er ikke mindre enn 50";
 		System.out.println(resulat);
+
+		// Switch
+		
+		switch (a) {
+		case 10:
+			System.out.println(a + " er 10");
+			break;
+		case 20:
+			System.out.println(a + " er 20");
+			break;
+		case 30:
+			System.out.println(a + " er 30");
+			break;	
+		case 40:
+			System.out.println(a + " er 40");
+			break;
+		case 60 :
+			System.out.println(a + " er 50");
+			break;	
+		default:
+			System.out.println(a + " er større enn 50");
+			break;
+		}
+	
+	
 	}
 
 }
