c2f98513361bd35d74aa8cebfceb9ae7e19ce900
terjebh <terjebh@W520>
2019-03-07 21:14:23 +0100

    JDBC test med PostgreSQL

---
+++ Postgres/src/no/itfakultetet/java/GetData.java
@@ -0,0 +1,28 @@
+package no.itfakultetet.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GetData {
+
+	public ResultSet getRows() {
+
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://s1.itfakultetet.no:5432/terje",
+				"terje", "terry432a")) {
+
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement
+					.executeQuery("SELECT concat(fornavn,' ',etternavn) as navn, epost,tel FROM public.kontakter");
+			return resultSet;
+
+		} catch (SQLException e) {
+			System.out.println("Noe gikk galt; ");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
