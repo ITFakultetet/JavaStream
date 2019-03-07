3bfc9a2c7361694c9a28d5ae8f2ee59b52fcca31
terjebh <terjebh@W520>
2019-03-07 15:14:23 +0100

    HashMap Eksempler

--- Maps/src/no/itfakultetet/java/HashMapDemo.java
+++ Maps/src/no/itfakultetet/java/HashMapDemo.java
@@ -12,33 +12,32 @@ public class HashMapDemo {
 
 		map1.put(6, "Nummer 6");
 		map1.put(1, "Nummer 1");
-		map1.put(11,"Nummer 11");
+		map1.put(11, "Nummer 11");
 		map1.put(3, "Nummer 3");
 		map1.put(2, "Nummer 2");
 		map1.put(12, "Nummer 12");
 
 		// Hent verdien til nøkkel 1
-		System.out.println(map1.get(1));
+		System.out.println("// Verdien til nøkkel 1 er: " + map1.get(1));
 
 		// Print ut toString() for map1
-		System.out.println(map1);
-		
+		System.out.println("\n// Dette er verdien til map.toString():\n" + map1.toString());
+
 		// Print ut et linjeskift
 		System.out.println();
 
+		// Gammel måte å bla gjennom en map på
+		System.out.println("// Gammel måte å bla gjennom en map på");
 		
-		// Gammel måte å bla gjennom en map
-
-		for(int key: map1.keySet()) {
-			System.out.println(key+ " : " + map1.get(key));
+		for (int key : map1.keySet()) {
+			System.out.println(key + " : " + map1.get(key));
 		}
-		
-		// Ny, enklere måte å bla gjennom en map på med lambda-funkasjon
+
+		// Ny, enklere måte å bla gjennom en map på med lambda-funksjon
+		System.out.println("\n// Ny, enklere måte å bla gjennom en map på med lambda-funksjon");
+
 		map1.forEach((k, v) -> System.out.println(k + " - " + v));
 
-		
-		
-		
 	}
 
 }
