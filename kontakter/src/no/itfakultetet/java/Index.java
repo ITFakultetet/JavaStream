4a6afc5b5a24b935b9bd79d2888ad8fa59b0db8c
terjebh <terjebh@W520>
2019-03-10 04:08:48 +0100

    Tomcat Servlet eksempel

--- kontakter/src/no/itfakultetet/java/Index.java
+++ kontakter/src/no/itfakultetet/java/Index.java
@@ -38,6 +38,10 @@ public class Index extends HttpServlet {
 		out.append("<html>");
 		out.append("<head>\n<title>Mine Kontakter</title>\n</head>");
 		out.append("<body>"); 
+		out.append("<style>"); 
+		out.append("td { padding:2px; spacing:1px; } ");
+		out.append("</style>");
+		
 		out.append("<h1>Mine Kontakter</h1>");
 		out.append("<p>Dette er mine kontakter som tabell</p>");
 
@@ -48,12 +52,22 @@ public class Index extends HttpServlet {
 			while (resultSet.next()) {
 				out.append("<tr><td>" + resultSet.getString("navn") + "</td>");
 				out.append("<td>" + resultSet.getString("epost") + "</td>");
-				out.append("<td>" + resultSet.getString("tel") + "</td></tr>");
+				out.append("<td>" + resultSet.getString("tel") + "</td></tr>");	
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		out.append("<table>");
+		
+		// Legg inn ny kontakt
+		out.append("<form name = \"regskjema\" method=\"post\" action=# ><br>");
+		out.append("Legg inn ny kontakt<br>");
+		out.append("Fornavn: <input type = \"text\" name=\"fornavn\"><br>");
+		out.append("Etternavn: <input type = \"text\" name=\"etternavn\"><br>");
+		out.append("Epost: <input type = \"text\" name=\"epost\"><br>");
+		out.append("Telefon: <input type = \"text\" name=\"tel\"><br>");
+		out.append("<input type = \"submit\"><br>");		
+
 		out.append("</body>");
 		out.append("</html>");
 
@@ -65,7 +79,14 @@ public class Index extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		out = response.getWriter();
+		
+		String fornavn = request.getParameter("fornavn");
+		String etternavn = request.getParameter("etternavn");
 		doGet(request, response);
+		out.append("Ditt fornavn er: "+fornavn+"<br>");
+		out.append("Ditt etternavn er: "+etternavn+"<br>");
 	}
 
 }
