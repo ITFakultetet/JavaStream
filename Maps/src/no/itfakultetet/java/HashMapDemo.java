53751b4812288413062eb995f22b32d2633cc2d3
terjebh <terjebh@W520>
2019-03-17 00:55:32 +0100

    Eksempler mewd Map

--- Maps/src/no/itfakultetet/java/HashMapDemo.java
+++ Maps/src/no/itfakultetet/java/HashMapDemo.java
@@ -18,25 +18,30 @@ public class HashMapDemo {
 		map1.put(12, "Nummer 12");
 
 		// Hent verdien til nøkkel 1
-		System.out.println("// Verdien til nøkkel 1 er: " + map1.get(1));
+		System.out.println("Verdien til nøkkel 1 er: " + map1.get(1));
 
 		// Print ut toString() for map1
-		System.out.println("\n// Dette er verdien til map.toString():\n" + map1.toString());
+		System.out.println("\nDette er verdien til map.toString():\n" + map1.toString());
 
 		// Print ut et linjeskift
 		System.out.println();
 
 		// Gammel måte å bla gjennom en map på
-		System.out.println("// Gammel måte å bla gjennom en map på");
+		System.out.println("Gammel måte å bla gjennom en map på");
 		
 		for (int key : map1.keySet()) {
 			System.out.println(key + " : " + map1.get(key));
 		}
 
 		// Ny, enklere måte å bla gjennom en map på med lambda-funksjon
-		System.out.println("\n// Ny, enklere måte å bla gjennom en map på med lambda-funksjon");
+		System.out.println("\nNy, enklere måte å bla gjennom en map på med stream api:");
 
-		map1.forEach((k, v) -> System.out.println(k + " - " + v));
+		map1.entrySet().forEach(System.out::println);
+		
+		// Eller med formatering
+		System.out.println("\nPrint ut map med formatering via lambda-funksjon:\n");
+		
+		map1.forEach((k, v) -> System.out.println("Nøkkel: "+ k + " har verdien: " + v));
 
 	}
 
