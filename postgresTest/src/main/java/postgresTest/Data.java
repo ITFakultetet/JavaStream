a0dc5c795ec61d30da3be828f7d7b5cb1e39e0e2
kursleder <kursleder@Kurs170.kurs.bouvet>
2019-10-09 18:39:33 +0200

    Brreg med Postgres

---
+++ postgresTest/src/main/java/postgresTest/Data.java
@@ -0,0 +1,60 @@
+package postgresTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class Data {
+
+	private Connection conn;
+	
+	public Data() {
+		String url = "jdbc:postgresql://s1.itfakultetet.no/brreg";
+		Properties props = new Properties();
+		props.setProperty("user","kurs");
+		props.setProperty("password","kurs123");
+//		props.setProperty("ssl","false");
+		
+		try {
+			conn = DriverManager.getConnection(url, props);
+		} catch (SQLException e) {
+			System.out.println("Forbindelse ikke akseptert fordi "+ e.getMessage()); 
+				e.printStackTrace();
+		}		
+		
+//		String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
+//		Connection conn = DriverManager.getConnection(url);
+	
+	} // end constructor
+	
+	
+	public void getData(String navn) {
+			
+		Statement st;
+		System.out.println("Orgnummer\tNavn");
+		System.out.println("-----------------------------------------");
+		
+		try {
+			st = conn.createStatement();
+			ResultSet rs = st.executeQuery("SELECT orgnr, navn FROM enheter WHERE navn ilike '"+ navn + "%'");
+			while (rs.next())
+			{
+			    
+			    System.out.println(rs.getString(1)+"\t"+rs.getString(2));
+			}
+			rs.close();
+			st.close();
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+
+}
