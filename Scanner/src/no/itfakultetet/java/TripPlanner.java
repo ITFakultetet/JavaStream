b29fadcf7fbfab8a1c8b36d7da3c868c0a9fb470
Terje Berg-Hansen <terje@itfakultetet.no>
2020-01-13 12:59:15 +0100

    En enkel reiseplanlegger

--- Scanner/src/no/itfakultetet/java/TripPlanner.java
+++ Scanner/src/no/itfakultetet/java/TripPlanner.java
@@ -21,7 +21,7 @@ public class TripPlanner {
         System.out.print("Hyggelig å treffe deg, " + nameOfPlanner + ". Hvor vil du reise? ");
         String travellingDestination = input.nextLine();
         System.out.println("Flott! Reise til " + travellingDestination + " høres ut som en fin tur");
-        System.out.println("*****\n\n");
+        System.out.println("*****\n");
 
     }
 
@@ -40,7 +40,7 @@ public class TripPlanner {
         System.out.println("Hvis du er borte i " + noOfDays + " dager, er det det samme som " + noOfDays * 24 + " timer, eller " + noOfDays * 24 * 60 + " minutter");
         System.out.println("Hvis du bruker " + (int) amountOfMoney + " NOK, kan du bruke " + (double) Math.round((amountOfMoney / noOfDays) * 100) / 100 + " NOK per dag");
         System.out.println("Ditt totale budsjett i " + symbolOfCurrency + " er " + amountOfMoney * multipleFactor + " " + symbolOfCurrency + ", som blir " + (double) Math.round((amountOfMoney * multipleFactor / noOfDays) * 100) / 100 + " " + symbolOfCurrency+" per dag");
-        System.out.println("*****\n\n\n");
+        System.out.println("*****\n");
 
     }
 
@@ -50,7 +50,7 @@ public class TripPlanner {
         System.out.print("Hva er tidsforskjellen, i timer, mellom hjemstedet ditt og destinasjonen? (sett minus foran hvis det er bakover i tid) ");
         int differenceOfTime = input.nextInt();
         System.out.println("Det betyr at når det er midnatt hjemme, vil klokken være " + (24 + differenceOfTime) % 24 + ":00 på destinasjonsstedet,\nog når kl. er 12 på dagen hjemme, vil den være " + (12 + differenceOfTime) + ":00 på destinasjonsstedet");
-        System.out.println("*****\n\n\n");
+        System.out.println("*****\n");
 
     }
 
