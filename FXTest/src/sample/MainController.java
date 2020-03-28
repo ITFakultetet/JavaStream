787e27b56065b37b9737fd21a7de10bdb847beec
Terje Berg-Hansen <terje@itfakultetet.no>
2020-03-28 05:01:42 +0100

    JavaFX Test

--- FXTest/src/sample/MainController.java
+++ FXTest/src/sample/MainController.java
@@ -1,8 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class MainController {
 
 
+    public Label helloWorld;
 
-
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
 }
