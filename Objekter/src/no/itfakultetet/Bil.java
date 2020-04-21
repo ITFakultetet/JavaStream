eb3dfd0273cd015b2b35ece19a05c6575893dbfc
Terje Berg-Hansen <terje@itfakultetet.no>
2020-04-21 14:53:29 +0200

    Objekter

---
+++ Objekter/src/no/itfakultetet/Bil.java
@@ -0,0 +1,14 @@
+package no.itfakultetet;
+
+public class Bil {
+
+    private String merke;
+    private String modell;
+    private Integer pris;
+
+    public Bil(String merke, String modell, Integer pris) {
+        this.merke = merke;
+        this.modell = modell;
+        this.pris = pris;
+    }
+}
