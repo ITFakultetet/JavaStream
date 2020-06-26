e33e5ea63a236d93f7f792cf833e84ca81162b2c
Terje Berg-Hansen <terje@itfakultetet.no>
2020-06-26 23:06:12 +0200

    Files.lines

--- Files/src/no/itfakultetet/App.java
+++ Files/src/no/itfakultetet/App.java
@@ -5,50 +5,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class App {
 
     public static void main(String[] args) {
+        // Hent ord fra fil, tell opp og putt i en TreeMap
 
-
-        List<String> ord = new ArrayList<>();
-
+        TreeMap<String, Long> ordListe = new TreeMap<>();
         try {
-            ord = Files.lines(Paths.get("war-and-peace.txt"))
+            ordListe = Files.lines(Paths.get("war-and-peace.txt"))
                     .map(a -> a.split("[0-9!*?'\"\t.,;:() ]"))
                     .flatMap(a -> Arrays.stream(a))
                     .filter(a -> a.length() > 1 & !a.contains("CHAPTER") & !a.contains("-"))
                     .map(a -> a.toLowerCase())
-                    //.limit(300)
-                    .collect(Collectors.toList());
-
+                    .collect(Collectors.groupingBy(a -> a,TreeMap::new,Collectors.counting()));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-//        Mulig, men ikke optimalt - Collectors.gr
-//        TreeMap<String,Long> ordListe = new TreeMap<>();
-//
-//        ord.forEach(
-//                a -> {
-//                    // Hvis ordet finnes på listen allerede, oppdater ordet med verdi = eksisterende verdi + 1
-//                    if (ordListe.containsKey(a)) {
-//                        ordListe.put(a, ordListe.get(a) + 1);
-//                    } else {
-//                        // Hvis ordet ikke finnes på listen, legg til ordet og sett verdien til 1
-//                        ordListe.put(a, 1);
-//                    }
-//                }
-//        );
-//
-
-        TreeMap<String, Long> ordListe = ord.stream().collect(Collectors.groupingBy(a -> a,TreeMap::new,Collectors.counting()));
-
+        // Skriv til fil
         try {
             FileWriter alfabetiskFil = new FileWriter("ord_alfabetisk.csv");
 
