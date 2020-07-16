72b74b364908ba3e008a70abd61b4951dd43bb57
Terje Berg-Hansen <terje@itfakultetet.no>
2020-07-16 17:03:29 +0200

    Files.lines

--- Files/src/no/itfakultetet/App.java
+++ Files/src/no/itfakultetet/App.java
@@ -1,27 +1,30 @@
 package no.itfakultetet;
 
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class App {
 
     public static void main(String[] args) {
-        // Hent ord fra fil, tell opp og putt i en TreeMap
 
-        TreeMap<String, Long> ordListe = new TreeMap<>();
+        // Hent ord fra en fil, tell opp og putt i en TreeMap, alfabetiskListe
+
+        Map<String, Long> alfabetiskListe = new TreeMap<>();
+        Map<String, Long> forekomstListe = new LinkedHashMap<>();
         try {
-            ordListe = Files.lines(Paths.get("war-and-peace.txt"))
+            alfabetiskListe = Files.lines(Paths.get("war-and-peace.txt"))
                     .map(a -> a.split("[0-9!*?'\"\t.,;:() ]"))
                     .flatMap(a -> Arrays.stream(a))
                     .filter(a -> a.length() > 1 & !a.contains("CHAPTER") & !a.contains("-"))
                     .map(a -> a.toLowerCase())
                     .collect(Collectors.groupingBy(a -> a,TreeMap::new,Collectors.counting()));
+            forekomstListe = alfabetiskListe.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(a, b)->a,LinkedHashMap::new));
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
@@ -29,31 +32,42 @@ public class App {
 
         // Skriv til fil
         try {
-            FileWriter alfabetiskFil = new FileWriter("ord_alfabetisk.csv");
+            FileWriter forekomstFil = new FileWriter("ord_forekomst.csv");
 
-            try {
-                alfabetiskFil.append("Ord,Antall\n");
-                ordListe.forEach(
-                        (k, v) -> {
-                            try {
-                                alfabetiskFil.append(k + "," + v + "\n");
-                            } catch (IOException e) {
-                                System.out.println("Fikk ikke appended til fil fordi: "+e.getMessage() );
-                                e.printStackTrace();
-                            }
-                        }
-                );
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            skrivTilFil("ord_alfabetisk.csv",alfabetiskListe);
+            skrivTilFil("ord_forekomst.csv",forekomstListe);
 
-            alfabetiskFil.flush();
-            alfabetiskFil.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
 
+    // Metode for å skrive ordlisene til fil
+    public static void skrivTilFil(String filNavn, Map<String,Long> liste ) {
+        try {
+
+            FileWriter fil = new FileWriter(filNavn);
+
+            fil.append("Ord,Antall\n");
+            liste.forEach(
+                    (k, v) -> {
+                        try {
+                            fil.append(k + "," + v + "\n");
+                        } catch (IOException e) {
+                            System.out.println("Fikk ikke appended til fil fordi: "+e.getMessage() );
+                            e.printStackTrace();
+                        }
+                    }
+            );
+            System.out.println(filNavn+" er lagret");
+            fil.flush();
+            fil.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+
 }
