d475319214ca692b2a89d036a79176ba61d23e2c
Terje Berg-Hansen <terje@itfakultetet.no>
2020-07-20 11:56:07 +0200

    Files

--- Files/src/no/itfakultetet/App.java
+++ Files/src/no/itfakultetet/App.java
@@ -3,7 +3,6 @@ package no.itfakultetet;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
